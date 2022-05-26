bd13706f259c22c4f56ef09399e8e59ddd007a56
mmkhajoo <mmkhajoo>
2022-05-26 10:03:38 +0430

    Jump Feel Good.

--- GameJam/Assets/Scripts/Player/PlayerMovement.cs
+++ GameJam/Assets/Scripts/Player/PlayerMovement.cs
@@ -6,14 +6,13 @@ namespace DefaultNamespace
     [RequireComponent(typeof(CharacterController2D))]
     public class PlayerMovement : MonoBehaviour
     {
-
         public event Action OnJump;
 
         public float VerticalMove => _verticalMove;
         public float HorizontalMove => _horizontalMove;
         public bool IsGrounded => controller.IsGrounded;
-        
-        
+
+
         public CharacterController2D controller;
 
         [SerializeField] private float runSpeed = 40f;
@@ -22,37 +21,60 @@ namespace DefaultNamespace
         private float _horizontalMove = 0f;
 
         private bool _jump = false;
+        private bool _waitForJumpButtonUp;
 
         private ConstantForce2D _constantForce2D;
-        
+
         //TODO : Add Events for Play Idle and Move Animation on Player
 
         private void Awake()
         {
             _constantForce2D = GetComponent<ConstantForce2D>();
-            
+
             controller.OnJumpAvailable += JumpAvailable;
         }
 
         private void JumpAvailable()
         {
-            _jump = false;
+            if (_jump)
+            {
+                _waitForJumpButtonUp = true;
+            }
         }
 
         private void Update()
         {
             _verticalMove = Input.GetAxisRaw("Vertical") * runSpeed;
             _horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-            
-          
+
             Debug.DrawLine(transform.position, transform.position + transform.right * 10, Color.yellow);
 
-            if (Input.GetButtonDown("Jump") && !_jump)
+            if (Input.GetButton("Jump") && !_waitForJumpButtonUp)
             {
                 _jump = true;
+            }
+            else
+            {
+                _jump = false;
+            }
+
+            if (Input.GetButtonDown("Jump") && controller.IsGrounded)
+            {
                 OnJump?.Invoke();
+
+                _waitForJumpButtonUp = false;
+            }
+
+
+            if (Input.GetButtonUp("Jump"))
+            {
+                if (controller.IsGrounded)
+                    _waitForJumpButtonUp = false;
+                else
+                    _waitForJumpButtonUp = true;
             }
         }
+
 
         private void FixedUpdate()
         {
@@ -60,12 +82,14 @@ namespace DefaultNamespace
             {
                 _verticalMove = 0;
             }
-            else if(_constantForce2D.force.x != 0f)
+            else if (_constantForce2D.force.x != 0f)
             {
                 _horizontalMove = 0;
             }
-            
-            controller.Move(_verticalMove * Time.fixedDeltaTime,_horizontalMove * Time.fixedDeltaTime,false,_jump);
+
+            controller.Move(_verticalMove * Time.fixedDeltaTime, _horizontalMove * Time.fixedDeltaTime, false, _jump);
+
+            Debug.Log("Jump = " + _jump);
         }
     }
 }
