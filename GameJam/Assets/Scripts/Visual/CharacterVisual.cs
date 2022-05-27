2e103e826a23fc87d67174b9b797a0a726455a94
Fazel <f.majidfar@gmail.com>
2022-05-27 20:42:52 +0430

    Level 1 Visual Completed

--- GameJam/Assets/Scripts/Visual/CharacterVisual.cs
+++ GameJam/Assets/Scripts/Visual/CharacterVisual.cs
@@ -6,18 +6,43 @@ public class CharacterVisual : MonoBehaviour
 {
    [SerializeField] private Animator animator;
    
+   [SerializeField] private GameObject characterGameObject;
+   
+   [SerializeField] private GameObject jumpParticle;
+   [SerializeField] private GameObject moveParticle;
+   [SerializeField] private GameObject landParticle;
+   [SerializeField] private GameObject dashParticle;
+   [SerializeField] private GameObject dieParticle;
+   
+   private static readonly int Speed = Animator.StringToHash("Speed");
    private static readonly int Jump = Animator.StringToHash("Jump");
    private static readonly int Walk = Animator.StringToHash("Walk");
    private static readonly int Idle = Animator.StringToHash("Idle");
    private static readonly int Land = Animator.StringToHash("Land");
+   // private static readonly int Dash = Animator.StringToHash("Land");
    
+   private void Start()
+   {
+      jumpParticle.SetActive(false);
+      moveParticle.SetActive(true);
+      landParticle.SetActive(false);
+      dashParticle.SetActive(false);
+      dieParticle.SetActive(false);
+   }
+
+   private void Update()
+   {
+      var speedX=Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x);
+      animator.SetFloat(Speed,speedX);
+   }
+
    public void OnPlayerStateChange(PlayerStateType state)
    {
       Debug.Log(state);
       switch (state)
       {
          case PlayerStateType.None:
-            DoIdle();
+          //  DoIdle();
             break;
          case PlayerStateType.Idle:
             DoIdle();
@@ -29,7 +54,7 @@ public class CharacterVisual : MonoBehaviour
             DoDie();
             break;
          default:
-            DoIdle();
+            //DoIdle();
             break;
       }
    }
@@ -48,23 +73,43 @@ public class CharacterVisual : MonoBehaviour
 
    private void DoWalk()
    {
+      moveParticle.SetActive(true);
       animator.SetTrigger(Walk);
    }
    private void DoJump()
    {
+      animator.ResetTrigger(Land);
       animator.SetTrigger(Jump);
+      
+      moveParticle.SetActive(false);
+      
+      jumpParticle.SetActive(false);
+      jumpParticle.SetActive(true);
    }
    private void DoIdle()
    {
+      moveParticle.SetActive(true);
       animator.SetTrigger(Idle);
    }
    private void DoLand()
    {
       animator.SetTrigger(Land);
+      
+      landParticle.SetActive(false);
+      landParticle.SetActive(true);
    }
    
    private void DoDie()
    {
+      characterGameObject.SetActive(false);
       
+      dieParticle.SetActive(false);
+      dieParticle.SetActive(true);
+   }
+   
+   private void DoDash()
+   {
+      dashParticle.SetActive(false);
+      dashParticle.SetActive(true);
    }
 }
